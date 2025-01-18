6f9d0ddf72811b5f47a7942e7564ca606eea49e0
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-01-18 16:44:16 +0530

    Basic Scanner program

---
+++ Demon.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Demon
+{
+int i;
+ double d;
+public static void main(String[] args)
+{
+char c = 'a';
+Demon obj = new Demon();
+Scanner s = new Scanner(System.in);
+d=s.nextDouble();
+System.out.println(obj.i);
+System.out.println(d);
+System.out.println(c);
+}
+}
