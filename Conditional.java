64906eb6374d9e981e5e4ce86a467ba3cac1fb90
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-01-18 16:34:59 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Condstat
+{
+public static void main(String[] args)
+{
+int a = 10;
+if ( a>10 )
+{
+System.out.println("Yes");
+}
+else
+{ 
+System.out.println("No");
+}
+}
+}
