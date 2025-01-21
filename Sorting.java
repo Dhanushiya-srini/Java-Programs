7a2d1a2b0d7983e725a6132ea4da214aeac3754e
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-01-21 20:46:48 +0530

    Sorting array using lambda Expressions

---
+++ Sorting.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class Sorting {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> num = new ArrayList<>(5);
+        num.add(50);
+        num.add(30);
+        num.add(20);
+        num.add(25);
+        num.add(40);
+        System.out.println("Elements of the ArrayList after" + " sorting : " + num);
+        Collections.sort(num, (o1, o2) -> (o1 > o2) ? -1 : (o1 < o2) ? 1 : 0);
+        System.out.println("Elements of the ArrayList after" + " sorting : " + num);
+    }  
+}
