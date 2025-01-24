5ad45871d2d57693329a51716b671a2f9cb0f08d
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-01-24 21:08:26 +0530

    Patterns

---
+++ Patterns.java
@@ -0,0 +1,28 @@
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("Increasing Triangle ");
+        for (int i = 1; i<=5; i++) {
+            for(int j=1;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    System.out.println("Decreasing Triangle ");
+    for (int i = 5; i>=1; i--) {
+        for(int j=1;j<=i;j++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+System.out.println("Pyramid Triangle ");
+for (int i= 1; i <= 5; i++) {
+    for ( int j = 1;j<=5-i; j++) {
+        System.out.print(" ");
+    }
+    for (int j = 1; j <= i; j++) {
+        System.out.print( "* ");
+    }
+    System.out.println();
+}
+}
+}
