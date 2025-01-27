733ca7053707f650f388ac86d35854b67d0cce24
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-01-27 21:27:12 +0530

    String and its method

---
+++ String.java
@@ -0,0 +1,13 @@
+ public class String {
+    public static void main(String[] args)
+    {
+        String str1 ="Hello";
+        String str2 ="How are you?";
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println("The length of the txt string is: " + str2.length());
+        System.out.println(str2.toUpperCase());   
+        System.out.println(str1.toLowerCase());
+    }
+ }
+
