c986c2ee9fec54ca21d2960b74e78a0406ab7d40
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-09 20:34:34 +0530

    Add basics for sum the first n odd numbers.

---
+++ J_084_Basic_SumFirstNOddNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class J_084_Basic_SumFirstNOddNumbers {
+     public static void main(String[] args) {
+        int sum=0;
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i=1;i<=2*num;i=i+2){
+            sum+=i;
+        }
+        System.out.println("The Sum of the First n Odd Number is "+sum);
+        scan.close();
+    }
+}
