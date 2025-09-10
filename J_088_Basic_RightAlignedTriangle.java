4a726d6d338be81694d47df6f636f86015724250
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-10 18:42:08 +0530

    Add basics for creating a right-aligned triangle pattern in Java.

---
+++ J_088_Basic_RightAlignedTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class J_088_Basic_RightAlignedTriangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=num-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
