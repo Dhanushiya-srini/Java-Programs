2ea724b8661352ae1500b5102649f65ebf931b96
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-11 19:12:08 +0530

    Refactor sum of squares calculation and add new program for sum of cubes in Java.

--- J_094_Basic_SumOfCubes.java
+++ J_094_Basic_SumOfCubes.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class J_093_Basic_SumOfSquares {
+public class J_094_Basic_SumOfCubes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         int sum=0;
         for(int i=1;i<=size;i++){
-            int sq = i*i;
-            sum+=sq;
+            int cube = i*i*i;
+            sum+=cube;
                 }
-        System.out.println("The Sum of the Squares of the numbers are: "+sum);
+        System.out.println("The Sum of the Cubes of the numbers are: "+sum);
         scan.close();
     }
 }
