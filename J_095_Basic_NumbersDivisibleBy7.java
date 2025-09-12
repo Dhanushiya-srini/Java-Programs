b98a307ea084b8509c8180aebb2e448580041f2b
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-12 21:01:29 +0530

    Add program to print numbers divisible by 7 from 1 to 500 in Java

---
+++ J_095_Basic_NumbersDivisibleBy7.java
@@ -0,0 +1,8 @@
+public class J_095_Basic_NumbersDivisibleBy7{
+    public static void main(String[] args) {
+        for(int i=1;i<=500;i++){
+            if(i%7==0)
+                System.out.print(i+",");
+        }
+    }
+}
