9e967f5ff527b9db582743cee9c4073016c7be78
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-13 21:21:26 +0530

    Add program to calculate compound interest in Java

---
+++ J_092_Basic_CompoundInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class J_092_Basic_CompoundInterest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int p = scan.nextInt();
+        double r = scan.nextDouble();
+        int n= scan.nextInt();
+        int t=scan.nextInt();
+        double mul=n*t;
+        double q =(1+(r/n));
+        double pow =Math.pow(q, mul);
+        double CI =p*pow-p;
+        System.out.println("The compound Interest is :"+CI);
+        scan.close();
+    }
+}
