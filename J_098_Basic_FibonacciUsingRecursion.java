e154a16ad3f3f629e586ccd1e2ded21c7a6692df
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-16 06:22:36 +0530

    Add program to calculate Fibonacci numbers using recursion in Java

---
+++ J_098_Basic_FibonacciUsingRecursion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class J_098_Basic_FibonacciUsingRecursion {
+    public static int Fibonacci(int num) {
+        if(num == 0)
+            return 0;
+         else if (num == 1) 
+            return 1;
+         else 
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num =scan.nextInt(); 
+        for (int i = 0; i < num; i++) {
+            System.out.print(Fibonacci(i) + " ");
+        }
+    scan.close();
+    }
+}
