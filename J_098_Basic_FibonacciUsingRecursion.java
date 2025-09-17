94407d7cd2fdf3c59fb3bb3c2b7b7ca90eec5d98
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-17 18:47:38 +0530

    Update J_098_Basic_FibonacciUsingRecursion.java

--- J_098_Basic_FibonacciUsingRecursion.java
+++ J_098_Basic_FibonacciUsingRecursion.java
@@ -13,7 +13,7 @@ public class J_098_Basic_FibonacciUsingRecursion {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num =scan.nextInt(); 
-        for (int i = 0; i < num; i++) {
+        for (int i = 0; i< num; i++) {
             System.out.print(Fibonacci(i) + " ");
         }
     scan.close();
