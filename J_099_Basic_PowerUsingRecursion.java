f553661f332084f6354aae3351b7a0749c6d7aa9
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-18 19:14:14 +0530

    Add a basics for calculating the power using recursion.

---
+++ J_099_Basic_PowerUsingRecursion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class J_099_Basic_PowerUsingRecursion{
+    public static int power(int num,int exp){
+        if(exp==0)
+            return 1;
+        else
+            return num*power(num,exp-1);
+    }
+
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    int exp = scan.nextInt();
+    System.out.println(power(num,exp));
+}
+}
