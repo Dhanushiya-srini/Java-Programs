d6454bcf0f017fc5074496e875656b63f8e6bc82
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-23 21:00:19 +0530

    Update J_002_Basic_ArithmeticOperations.java

--- J_002_Basic_ArithmeticOperations.java
+++ J_002_Basic_ArithmeticOperations.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class J_002_Basic_ArithmeticOperations{
     public static void main(String[] args){
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int num1=scan.nextInt();
         int num2=scan.nextInt();
         int add= num1+num2;
