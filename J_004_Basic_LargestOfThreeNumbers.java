971f89f6701fd1b1ae2a9a042a600494658878fa
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-09-26 21:50:47 +0530

    Update J_004_Basic_LargestOfThreeNumbers.java

--- J_004_Basic_LargestOfThreeNumbers.java
+++ J_004_Basic_LargestOfThreeNumbers.java
@@ -6,7 +6,7 @@ public class J_004_Basic_LargestOfThreeNumbers {
         System.out.println("Enter the number1: ");
         int num1=scan.nextInt();
          System.out.println("Enter the number2: ");
-        int num2=scan.nextInt();
+        int num2 =scan.nextInt();
          System.out.println("Enter the number3: ");
         int num3=scan.nextInt();
         if(num1==num2&&num2==num3){
