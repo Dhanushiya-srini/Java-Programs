13a833d8b606d7ff91f6ebff8ce15716ef94035e
Dhanushiya Srinivasan <dhanush.srini11@gmail.com>
2025-10-11 21:37:37 +0530

    Update J_084_Basic_SumFirstNOddNumbers.java

--- J_084_Basic_SumFirstNOddNumbers.java
+++ J_084_Basic_SumFirstNOddNumbers.java
@@ -5,7 +5,7 @@ public class J_084_Basic_SumFirstNOddNumbers {
         int sum=0;
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
-        for(int i=1;i<=2*num;i=i+2){
+        for(int i =1;i<=2*num;i=i+2){
             sum+=i;
         }
         System.out.println("The Sum of the First n Odd Number is "+sum);
